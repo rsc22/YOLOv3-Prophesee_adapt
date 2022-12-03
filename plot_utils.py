1385ce880d300478d482778548aca80ecca800c1
rsc_22 <ricardo.sua.car@gmail.com>
2022-12-03 20:11:23 +0100

    Add plotting image function

--- plot_utils.py
+++ plot_utils.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import time
 
 def plot_losses(train_loss, test_loss, epoch):
@@ -8,3 +9,13 @@ def plot_losses(train_loss, test_loss, epoch):
     plt.show(block=False)
     time.sleep(2)
     plt.close(fig)
+
+def plot_img(imgs, ax=None):
+    if not ax:
+        fig, ax = plt.subplots()
+    im = np.transpose(imgs[0,...].cpu(), (1, 2, 0))
+    #im = np.transpose(im.cpu(), (1, 2, 0))
+    ax.imshow(im)
+    plt.axis('off')
+    plt.close()
+    return ax
